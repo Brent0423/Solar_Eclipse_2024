bf90060e117e4882b0b5d46bc4ea7e411bc5f1f4
Brent0423 <bsan23@gmail.com>
2024-03-20 18:16:49 -0400

    flask update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,6 +10,6 @@ def home():
 
 # Add any additional routes as needed
 
-if __name__ == '__main__':
-    # This condition checks if the script is being run directly and starts the Flask server
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use port provided by Heroku or default to 5000
+    app.run(host="0.0.0.0", port=port)
